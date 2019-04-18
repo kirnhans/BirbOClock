2200e86ebeaeaeb85093a3fcbdd536ee6b5ee290
soundofez <49661366+soundofez@users.noreply.github.com>
2019-04-18 00:28:12 -0500

    Update birbFez.py

--- birbFez.py
+++ birbFez.py
@@ -5,33 +5,50 @@ import re
 import random
 
 TOKEN = 'MzM4MDc5MzU5NjczODI3MzI5.XKaguA.x02mZO1c59_dDCradPhfBFDbWrg'
+BOT_PREFIX = ('f#', 'f!')
+DESCRIPT = 'A work in progress. Mostly just reacts to its name.'
 
 class MyBot(discord.Client):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-		#create the background task and run it in the background
-		self.bg_task = self.loop.create_task(self.my_background_task())
+		# running background tasks
+		self.bg_task = self.loop.create_task(self.loop_games())
 
 	async def on_ready(self):
 		print('Logged in as ' + self.user.name + "#" + str(self.user.id))
 		print('-------')
 
-	async def my_background_task(self):
+	# speak of the birbfez and it shall appear
+	async def on_message(self, message):
+		if message.author == self.user:
+			return
+		if re.search('birbfez', message.content, re.IGNORECASE):
+			print('{0.author.name}#{0.author.id} mentioned birbfez in #{0.channel.name} ({0.guild.name})'.format(message))
+			print("-------")
+			emoji = ['\N{EYES}', '\N{THUMBS UP SIGN}', '\N{HATCHING CHICK}', '\N{BIRD}']
+			mojinum = len(emoji) - 1
+			await message.add_reaction(emoji[random.randint(0,mojinum)])
+			return
+
+	# background tasks:
+	# loop_games(self) -> cycle through preset game statuses
+	async def loop_games(self):
 		await self.wait_until_ready()
-		counter = 0
-		channel = self.get_channel(563660295373848607)	#talk-with-birbfez (I Like Your BOT)
+		games = ["without supervision", "with humans", "Python 3", ]
 		while not self.is_closed():
-			counter += 1
-			await channel.send(counter)
-			await asyncio.sleep(5)	#task runs every 5 seconds
+			try:
+				for game in games:
+					print('Playing ' + game)
+					print("-------")
+					await self.change_presence(activity=discord.Game(name=game))
+					await asyncio.sleep(5)	# changes game every 5 seconds
+			except:
+				print('birbfez experienced a gaming mishap.')
 
 
 bot = MyBot()
 bot.run(TOKEN)
-
-BOT_PREFIX = ('f#', 'f!')
-DESCRIPT = 'A work in progress. Mostly just reacts to its name.'
 
 # bot = commands.Bot(command_prefix=BOT_PREFIX, description=DESCRIPT)
 # bot.load_extension('cogBasic')
@@ -101,18 +118,3 @@ DESCRIPT = 'A work in progress. Mostly just reacts to its name.'
 # 			await channel.send(guilds.name)
 # 		print("-------")
 # 		await asyncio.sleep(600)
-
-# async def loop_games():
-# 	await bot.wait_until_ready()
-# 	activities = ["without supervision", "death-defying marital bliss", "with the code", "with humans"]
-# 	while not bot.is_closed:
-# 		for n in range(len(activities)):
-# 			try:
-# 				await bot.change_presence(activity=discord.GameGame(name=activities[n]))
-# 			except:
-# 				print("Websocket weirdness.")
-# 			await asyncio.sleep(5)
-
-# bot.loop.create_task(list_servers())
-# bot.loop.create_task(loop_games())
-# bot.run(TOKEN, bot=True, reconnect=True)
