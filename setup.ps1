bdcd3d401e510e43ed233e2e520cd0eb61bf4be3
Lucas Kent <rubickent@gmail.com>
2022-12-26 14:13:09 +1100

    Fix windows scripts

--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,7 @@ Remove-Item venv -Recurse -ErrorAction Ignore
 python -m venv venv
 ./venv/Scripts/Activate.ps1
 pip install -r requirements.txt
+
+if (Get-Command "deactivate" -errorAction SilentlyContinue) {
+    deactivate
+}
