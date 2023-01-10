04388cc267b6e8b63e76d59f11b4812e1957315c
Kirn Hans <kirn.hans@gmail.com>
2023-01-10 17:26:56 +0530

    Aliases for b!pins

--- cogInfo.py
+++ cogInfo.py
@@ -12,7 +12,8 @@ class InfoCog(commands.Cog, name="Informational Commands"):
 		self.bot = bot
 
 	# pins - Reports number of pins in the channel
-	@commands.command(description="Reports the number of pins in the channel in which it is called.", alias="pin")
+	@commands.command(description="Reports the number of pins in the channel in which it is called.",
+	 aliases=["pin","pinned"])
 	async def pins(self, ctx):
 		pins = str(len(await ctx.channel.pins()))
 		await ctx.send("There are " + pins + "/50 pins in this channel.")
